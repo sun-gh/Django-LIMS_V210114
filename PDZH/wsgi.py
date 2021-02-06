26d17ff0cf1a8a975e654aacf02301040c9e2ca3
sun-gh <sun224@163.com>
2021-02-07 00:02:06 +0800

    modify datatable column width

--- PDZH/wsgi.py
+++ PDZH/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+# import sys
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(BASE_DIR)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PDZH.settings')
